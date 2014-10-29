8741cce1108d4f3e39d7565df32c3b9ed6a3b565
Lihis <lihis@lihis.net>
2014-10-29 09:21:10 +0200

    CHANGE: omnified

--- omni_u8860.mk
+++ omni_u8860.mk
@@ -6,6 +6,6 @@ $(call inherit-product, device/huawei/u8860/device_u8860.mk)
 
 # Device identifier.
 PRODUCT_DEVICE := u8860
-PRODUCT_NAME := u8860
+PRODUCT_NAME := omni_u8860
 PRODUCT_MODEL := U8860
 PRODUCT_MANUFACTURER := Huawei
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo u8860-userdebug
+add_lunch_combo omni_u8860-userdebug
